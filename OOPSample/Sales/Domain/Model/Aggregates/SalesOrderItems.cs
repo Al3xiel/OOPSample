6a832b7a9ec6a13da5ac12e71263ac784c3c9632
Al3xiel <gabriel280104casas@gmail.com>
2024-08-22 16:39:47 -0500

    feat(sales-order): added sales order item entity.

---
+++ OOPSample/Sales/Domain/Model/Aggregates/SalesOrderItems.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics.Contracts;
+
+namespace OOPSample.Sales.Domain.Model.Aggregates;
+
+public class SalesOrderItems
+{
+    public SalesOrderItems(Guid salesOrderId, int productId, int quantity, double unitPrice)
+    {
+        if (salesOrderId == Guid.Empty)
+        {
+            throw new ArgumentException("SalesOrderId cannot be empty");
+        }
+        if (productId <= 0)
+        {
+            throw new ArgumentException("Product ID is required");
+        }
+
+        if (quantity <= 0)
+        {
+            throw new ArgumentException("Quantity must be grater than zero");
+        }
+        if (unitPrice <= 0)
+        {
+            throw new ArgumentException("Quantity must be grater than zero");
+        }
+        SalesOrderId = salesOrderId;
+        ProductId = productId;
+        Quantity = quantity;
+        UnitPrice = unitPrice;
+    }
+
+    public double CalculateItemPrice() => Quantity * UnitPrice;
+    
+    public Guid Id { get; private set; } = GenerateOrderItemId();
+    public Guid SalesOrderId { get;}
+    public int ProductId { get;}
+    public int Quantity { get;}
+    public double UnitPrice { get;}
+
+    private static Guid GenerateOrderItemId()
+    {
+        return Guid.NewGuid();
+    }
+}
